fc5da8019745e493d3bac01e27fadc0580adc178
gulyolik <olia.guliaeva@yandex.ru>
2022-10-16 17:22:07 +0300

    Fixes

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         int balance = 17;
-        int x = 1345;
-        int balancefin = balance + x;
+        int add = 1345;
+        int balancefin = balance + add;
         int bonus;
         bonus = balancefin / 100;
-        if(balancefin > 1000) {System.out.println("Итоговый счёт:" + balancefin + ". Количество бонусных рублей: " + bonus);}
-        else {System.out.println("Итоговый счёт: " + balancefin);}
-
-
+        if (balancefin > 1000) {
+            System.out.println("Итоговый счёт:" + balancefin + ". Количество бонусных рублей: " + bonus);
+        } else {
+            System.out.println("Итоговый счёт: " + balancefin);
+        }
 
 
     }
